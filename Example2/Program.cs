cb892ed26ce6e2e2df1183d6f01a55bed365cd6c
Denis Zorich <zorichd@gmail.com>
2022-09-16 23:37:39 +0300

    Решение условий задачи 2

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,2 +1,13 @@
 ﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+i = max;
+
+if(num1 = max);
+else (na)
